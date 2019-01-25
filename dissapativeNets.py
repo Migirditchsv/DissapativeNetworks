067c9ecb54932cdfc47b958d6b3ff4d2435a5367
Migirditchsv <33702890+Migirditchsv@users.noreply.github.com>
2019-01-25 17:15:28 -0500

    Ahhhh sorry for the confusion.
    I need to start developing my personal git.

---
+++ dissapativeNets.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+How does capitalism end?
+A simulation of dissapation driven percolation in competitive networks, and how
+the developed structures decay once the dissapative rescource runs out.
+Python 3.7
+By Sam Migirditch and Thomas Varley
+samigir@iu.edu
+
+To Do:
+
+1) Define Rescource, Producer and Consumer classes
+2) Define base versions of functions for node spawning, prefferential
+attachment, rescource consumption, rescource output and node death.
+3) Define main loop.
+4) Get on NPR
+    
+Change Log:
+01/25 SVM: Created
+"""
+
